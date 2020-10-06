43caf1af0f3c144327576a04d1790644729b2dd9
Kevin Davis <kevincarrolldavis@gmail.com>
2020-10-06 17:39:50 -0400

    PeripheralManagerDelegate update
    Forgot to await coroutine

--- bleak/backends/corebluetooth/PeripheralManagerDelegate.py
+++ bleak/backends/corebluetooth/PeripheralManagerDelegate.py
@@ -93,7 +93,7 @@ class PeripheralManagerDelegate(NSObject):
         while not self._advertisement_started:
             if not self.ready:
                 await self.is_ready()
-                return self.startAdvertising_(advertisement_data)
+                return await self.startAdvertising_(advertisement_data)
             await asyncio.sleep(0.01)
 
         logger.debug(
