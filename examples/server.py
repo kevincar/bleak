c37327312beb204252d941396e512f2f8b843950
Kevin Davis <kevincarrolldavis@gmail.com>
2020-10-08 12:41:35 -0400

    Minor updates

--- examples/server.py
+++ examples/server.py
@@ -17,28 +17,43 @@ logger = logging.getLogger(name=__name__)
 my_service_name = "ECoGLink"
 my_service_uuid = "A07498CA-AD5B-474E-940D-16F1FBE7E8CD"
 
-def read_request(characteristic: BleakGATTCharacteristic, **kwargs) -> bytearray:
+
+def read_request(
+        characteristic: BleakGATTCharacteristic,
+        **kwargs
+        ) -> bytearray:
     logger.debug(f"DANG {characteristic.value}")
     return characteristic.value
 
-def write_request(characteristic: BleakGATTCharacteristic, value: Any, **kwargs):
+
+def write_request(
+        characteristic: BleakGATTCharacteristic,
+        value: Any,
+        **kwargs
+        ):
     characteristic.set_value(value)
     logger.debug(f"Char value set to {characteristic.value}")
+
 
 async def run(loop):
     server = BleakServer(name=my_service_name, loop=loop)
     await server.add_new_service(my_service_uuid)
-    await server.add_new_characteristic(my_service_uuid, 
-        "51FF12BB-3ED8-46E5-B4F9-D64E2FEC021B", 
-        GattCharacteristicsFlags.read.value | GattCharacteristicsFlags.write.value | GattCharacteristicsFlags.indicate.value, 
-        None,
-        0x1 | 0x2)
+    await server.add_new_characteristic(
+            my_service_uuid,
+            "51FF12BB-3ED8-46E5-B4F9-D64E2FEC021B",
+            GattCharacteristicsFlags.read.value |
+            GattCharacteristicsFlags.write.value |
+            GattCharacteristicsFlags.indicate.value,
+            None,
+            0x1 | 0x2)
+
 
     server.read_request_func = read_request
     server.write_request_func = write_request
 
     await server.start()
     logger.debug("Advertising")
+    logger.debug(f"Services: {server.services.services[my_service_uuid].get_characteristic('51FF12BB-3ED8-46E5-B4F9-D64E2FEC021B')}")
     await asyncio.sleep(20)
     logger.debug("Updating")
     server.updateValue(my_service_uuid, "51FF12BB-3ED8-46E5-B4F9-D64E2FEC021B")
