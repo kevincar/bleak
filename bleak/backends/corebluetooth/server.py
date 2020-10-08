cc8ddfe9cea0450bd0e6c543fbec53c7c84e0b4a
Kevin Davis <kevincarrolldavis@gmail.com>
2020-10-08 12:40:26 -0400

    Updates without the Application Class

--- bleak/backends/corebluetooth/server.py
+++ bleak/backends/corebluetooth/server.py
@@ -6,6 +6,13 @@ Created on 2019-07-03 by kevincar <kevincarrolldavis@gmail.com>
 
 import logging
 
+from typing import (
+        Callable,
+        Dict,
+        Any,
+        Optional
+        )
+from asyncio.exceptions import TimeoutError
 from asyncio.events import AbstractEventLoop
 
 from Foundation import CBUUID, CBMutableService, CBMutableCharacteristic
@@ -13,9 +20,17 @@ from Foundation import CBUUID, CBMutableService, CBMutableCharacteristic
 from bleak.exc import BleakError
 from bleak.backends.server import BaseBleakServer
 from bleak.backends.characteristic import GattCharacteristicsFlags
-from bleak.backends.corebluetooth import Application
-from bleak.backends.corebluetooth.service import BleakGATTServiceCoreBluetooth, BleakGATTServiceCollectionCoreBluetooth
-from bleak.backends.corebluetooth.characteristic import BleakGATTCharacteristicCoreBluetooth
+from bleak.backends.corebluetooth.service import (
+        BleakGATTServiceCoreBluetooth,
+        BleakGATTServiceCollectionCoreBluetooth
+        )
+from bleak.backends.corebluetooth.characteristic import (
+        BleakGATTCharacteristicCoreBluetooth
+        )
+
+from .PeripheralManagerDelegate import (
+        PeripheralManagerDelegate
+        )
 
 logger = logging.getLogger(name=__name__)
 
@@ -26,29 +41,27 @@ class BleakServerCoreBluetooth(BaseBleakServer):
     def __init__(self, name: str, loop: AbstractEventLoop = None, **kwargs):
         super(BleakServerCoreBluetooth, self).__init__(loop=loop, **kwargs)
 
-        self.app = Application(client=False, loop=self.loop)
         self.name = name
-        self.services = BleakGATTServiceCollectionCoreBluetooth()
-        self.read_request_func = None
-        self.write_request_func = None
+        self.services: BleakGATTServiceCollectionCoreBluetooth = (
+                BleakGATTServiceCollectionCoreBluetooth()
+                )
 
-    def __del__(self):
-        self.app.__del__()
+        self.callbacks: Dict[str, Callable] = {}
 
-    async def is_ready(self):
-        await self.app._is_delegate_ready()
-        self.app.peripheral_manager_delegate.readRequestFunc = self.read_request
-        self.app.peripheral_manager_delegate.writeRequestsFunc = self.write_request
-        logger.debug("BleakServer is ready")
+        self.peripheral_manager_delegate: PeripheralManagerDelegate = (
+                PeripheralManagerDelegate.alloc().init()
+                )
+        self.peripheral_manager_delegate.readRequestFunc = self.read_request
+        self.peripheral_manager_delegate.writeRequestsFunc = self.write_request
 
     async def start(self):
-        await self.is_ready()
+        await self.peripheral_manager_delegate.wait_for_powered_on(10)
 
         for service_uuid in self.services.services:
             bleak_service = self.services.get_service(service_uuid)
             service_obj = bleak_service.obj
             print(f"Adding service: {bleak_service.uuid}")
-            await self.app.peripheral_manager_delegate.addService_(service_obj)
+            await self.peripheral_manager_delegate.addService_(service_obj)
 
         if not self.read_request_func or not self.write_request_func:
             raise BleakError("Callback functions must be initialized first")
@@ -58,34 +71,38 @@ class BleakServerCoreBluetooth(BaseBleakServer):
             "kCBAdvDataLocalName": self.name
             }
         logger.debug("Advertisement Data: {}".format(advertisement_data))
-        await self.app.peripheral_manager_delegate.startAdvertising_(advertisement_data)
+        try:
+            await self.peripheral_manager_delegate.startAdvertising_(advertisement_data)
+        except TimeoutError:
+            await self.start()
+
         logger.debug("Advertising...")
 
     async def stop(self):
-        await self.app.peripheral_manager_delegate.stopAdvertising()
+        await self.peripheral_manager_delegate.stopAdvertising()
 
     def is_connected(self) -> bool:
         """
         Determine whether there are any connected peripheral devices
         """
-        n_subscriptions = len(self.app.peripheral_manager_delegate._central_subscriptions)
+        n_subscriptions = len(self.peripheral_manager_delegate._central_subscriptions)
         return n_subscriptions > 0
 
     def is_advertising(self) -> bool:
         """
         Determine whether the service is advertising
         """
-        return self.app.peripheral_manager_delegate.is_advertising() == 1
-    
+        return self.peripheral_manager_delegate.is_advertising() == 1
+
     async def add_new_service(self, _uuid: str):
         """
         Add a service and all it's characteristics to be advertised
         """
         logger.debug("Creating a new service with uuid: {}".format(_uuid))
 
-        service_uuid = CBUUID.alloc().initWithString_(_uuid)
-        cb_service = CBMutableService.alloc().initWithType_primary_(service_uuid, True)
-        bleak_service = BleakGATTServiceCoreBluetooth(obj=cb_service)
+        service_uuid: CBUUID = CBUUID.alloc().initWithString_(_uuid)
+        cb_service: CBMutableService = CBMutableService.alloc().initWithType_primary_(service_uuid, True)
+        bleak_service: BleakGATTServiceCoreBluetooth = BleakGATTServiceCoreBluetooth(obj=cb_service)
 
         self.services.add_service(bleak_service)
 
@@ -94,8 +111,8 @@ class BleakServerCoreBluetooth(BaseBleakServer):
         Generate a new characteristic to be associated with the server
         """
         logger.debug("Craeting a new characteristic with uuid: {}".format(char_uuid))
-        cb_uuid = CBUUID.alloc().initWithString_(char_uuid)
-        cb_characteristic = CBMutableCharacteristic.alloc().initWithType_properties_value_permissions_(cb_uuid, properties, value, permissions)
+        cb_uuid: CBUUID = CBUUID.alloc().initWithString_(char_uuid)
+        cb_characteristic: CBMutableCharacteristic = CBMutableCharacteristic.alloc().initWithType_properties_value_permissions_(cb_uuid, properties, value, permissions)
         bleak_characteristic = BleakGATTCharacteristicCoreBluetooth(obj=cb_characteristic)
         self.services.get_service(service_uuid).add_characteristic(bleak_characteristic)
 
@@ -107,17 +124,58 @@ class BleakServerCoreBluetooth(BaseBleakServer):
         characteristic.set_value. This send notifications to subscribed
         central devices.
         """
-        characteristic: BleakGATTCharacteristicCoreBluetooth = self.\
-            services.characteristics[char_uuid]
+        characteristic: BleakGATTCharacteristicCoreBluetooth = (
+                self.services.characteristics[char_uuid.lower()]
+                )
 
         value: bytes = characteristic.value
         value = value if value is not None else b'\x00'
-        result: bool = self.app.peripheral_manager_delegate.\
-            peripheral_manager.\
-            updateValue_forCharacteristic_onSubscribedCentrals_(
-                    value,
-                    characteristic.obj,
-                    None
+        result: bool = (
+                self.peripheral_manager_delegate
+                    .peripheral_manager
+                    .updateValue_forCharacteristic_onSubscribedCentrals_(
+                        value,
+                        characteristic.obj,
+                        None
+                        )
                     )
 
         return result
+
+    @property
+    def read_request_func(self) -> Callable:
+        """
+        Return an instance of the function to handle incoming read requests
+        """
+        func: Optional[Callable[[Any], Any]] = self.callbacks.get('read')
+        if func is not None:
+            return func
+        else:
+            raise BleakError("Server: Read Callback is undefined")
+
+    @read_request_func.setter
+    def read_request_func(self, func: Callable):
+        """
+        Set the function to handle incoming read requests
+        """
+        self.callbacks['read'] = func
+
+    @property
+    def write_request_func(self) -> Callable:
+        """
+        Return an instance of the function to handle incoming write requests
+        """
+        func: Optional[Callable[[Any], Any]] = self.callbacks.get('write')
+        if func is not None:
+            return func
+        else:
+            raise BleakError("Server: Write Callback is undefined")
+
+    @write_request_func.setter
+    def write_request_func(self, func: Callable):
+        """
+        Set the function to handle incoming write requests
+        """
+        self.callbacks['write'] = func
+
+
